9d45f4b295c788f7aee1d1e1e0ded1b716b7f853
AbdielP <abdiel.pinzonc@gmail.com>
2021-07-08 18:45:42 -0500

    Servidor base de nodejs con express. se remueve comentario de cadena de conexión mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,3 @@ app.use(require('./routes/index'));
 app.listen(port, () => {
     console.log(`Servidor BACKEND en puerto: ${port}`)
 });
-
-// mongodb+srv://fastadmin:*****@cluster0.8u4oh.mongodb.net/test?authSource=admin&replicaSet=atlas-1la8fj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=trueS
