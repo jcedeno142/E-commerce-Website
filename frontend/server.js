a78c3669e7a68dd11481c8f1e20f70693e0c90f4
AbdielP <abdiel.pinzonc@gmail.com>
2021-07-15 15:46:28 -0500

    Estructura básica de frontend con servidor express

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./routes/index'));
+
+app.listen(port, () => {
+    console.log(`Servidor BACKEND en puerto: ${port}`)
+});
